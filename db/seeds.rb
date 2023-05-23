b68b1b519e05c5873ea8a5def408009881bfbb70
ricardo martins <ricardomfneves@gmail.com>
2023-05-23 17:03:57 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 10)
+  )
+  article.save!
+end
